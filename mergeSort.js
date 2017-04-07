a5edd319e7a315548f679ba6b088eb47804b84dc
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-04-07 11:24:29 -1000

    haha

--- mergeSort.js
+++ mergeSort.js
@@ -1,51 +1,47 @@
+
+
 /* jshint esversion: 6 */
 var mergeSort = (arr) => {
-  if(arr.length <= 1){
-    console.log("hiho",arr);
+  //base case if the arr is length 1 or 0
+  if (arr.length<2){
+    console.log(arr);
     return arr;
   }
 
-    var halfpoint = parseInt((arr.length) / 2);
-    var left   = arr.slice(0, halfpoint);
-    var right  = arr.slice(halfpoint, arr.length);
+  // 1) pick a pivot in the middle of the arr
+  const pivot = Math.ceil(arr.length/2);
+  // 2) spilt arr in the half at the pivot
+  const pleft= arr.slice(0,pivot);
+  const pright = arr.slice(pivot, arr.length);
 
-console.log("left",left);
-console.log("right",right);
+  //3) recursivly call merge sort until we have
+  //completely split up the arr
+  const left = mergeSort(pleft);
+  const right = mergeSort(pright);
 
-return merge(mergeSort(left),mergeSort(right));
+  //4 once we have broken it up, merge the the split halves back up
+  return merge(left,right);
+
 };
-
-function merge(left, right)
-{
-    var result = [];
-
-    while (left.length && right.length) {
-        if (left[0] <= right[0]) {
-            result.push(left.shift());
-
-        } else {
-            result.push(right.shift());
-        }
-        console.log("left",left);
-        console.log("right",right);
-    }
-
-    while (left.length>0){
-        result.push(left.shift());
-        console.log("leftl",left);
-        console.log("rightl",right);
-    }
-
-    while (right.length>0){
-        result.push(right.shift());
-        console.log("leftr",left);
-        console.log("rightr",right);
-    }
-    return result;
-}
-
-
-
-
-
 module.exports = mergeSort;
+
+const merge = (left,right) => {
+  const result = [];
+  while (left.length > 0 && right.length > 0){
+    if(left[0] < right[0]){
+      result.push(left.shift());
+    }else{
+      result.push(right.shift());
+    }
+  }
+//while there are still elements in left do this
+  while(left.length>0){
+    result.push(left.shift());
+  }
+
+  while(right.length>0){
+    result.push(right.shift());
+  }
+  return result;
+console.log(mergeSort(arr));
+}
